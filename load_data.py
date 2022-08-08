6ecb741b3c7fb480cd8f2ff8ac4fdb0add3887a5
Леонтьев Арсений Евгеньевич <arseniy.leontiev@volnamobile.ru>
2022-08-08 19:07:58 +0400

    Инициализация проекта

---
+++ load_data.py
@@ -0,0 +1,12 @@
+import csv
+
+def main():
+    file_path = "police_data.csv"
+    with open(file_path, "r", newline="") as f:
+        reader = csv.reader(f, delimiter=",", quotechar="|")
+        for row in reader:
+            pass
+
+if __name__ == "__main__":
+    main()
+ 
