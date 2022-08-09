a0634d13b1777cc04f43cd4e7bfd2e660e7ac219
Леонтьев Арсений Евгеньевич <arseniy.leontiev@volnamobile.ru>
2022-08-09 13:51:05 +0400

    Эндоинты для получения Report

--- reports/urls.py
+++ reports/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from reports.views import ReportRetrieveView, ReportRetrieveViewTest
+
 urlpatterns = [
-    # path("", ..., ...)
+    path("", ReportRetrieveView.as_view(), name="reports-retrieve"),
+    path("test/", ReportRetrieveViewTest.as_view(), name="reports-retrieve")
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
 
-# Create your views here.
+from reports.serializers import ReportOutputSerializer
+
+from .models import Report
+
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 20
+
+
+class ReportRetrieveView(APIView):
+
+    def get(self, request):
+        query = Report.objects.all()
+        params = request.GET
+        date_from, date_to = params.get("date_from"), params.get("date_to")
+        if date_from and date_to:
+            query.filter(report_date__gte=date_from, report_date__lte=date_to)
+
+        paginator = CustomPageNumberPagination()
+        paginated_queryset = paginator.paginate_queryset(query, request)
+        response = ReportOutputSerializer(paginated_queryset, many=True)
+        return paginator.get_paginated_response(response.data)
+
+
+class ReportRetrieveViewTest(ListAPIView):
+    """
+    Я, в своих проектах, не использую дженерики, по типу ListApiView, ListCreateApiView и подобные, для большего
+    контроля. Написал для справки, что бы было понимание, что я знаю про их существование =)
+    """
+    pagination_class = CustomPageNumberPagination
+    serializer_class = ReportOutputSerializer
+
+    def get_queryset(self):
+        query = Report.objects.all()
+        params = self.request.GET
+        date_from, date_to = params.get("date_from"), params.get("date_to")
+        if date_from and date_to:
+            query.filter(report_date__gte=date_from, report_date__lte=date_to)
+
+        return query
